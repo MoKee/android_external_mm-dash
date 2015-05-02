6febeac659366cf182fe78cc31aadb7372daab33
dianlujitao <dianlujitao@gmail.com>
2015-05-02 08:58:08 +0800

    mm-dash: only build for msm8916 and msm8909

--- Android.mk
+++ Android.mk
@@ -1,5 +1,7 @@
+ifneq ($(filter msm8916 msm8909,$(TARGET_BOARD_PLATFORM)),)
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 include $(QCOM_MEDIA_ROOT)/QCMediaPlayer/Android.mk
 include $(QCOM_MEDIA_ROOT)/dashplayer/Android.mk
+endif
 
