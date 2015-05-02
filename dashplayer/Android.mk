5da8103d5d5378cc103984baad9d8ca97706ce3c
dianlujitao <dianlujitao@gmail.com>
2015-05-02 10:19:25 +0800

    Revert "mm-dash: Support for closed captioning data"
    This reverts commit 3c2fc2878e5be3be363890d36f59a9b36fe332b2.

--- dashplayer/Android.mk
+++ dashplayer/Android.mk
@@ -41,17 +41,11 @@ LOCAL_C_INCLUDES := \
 	$(TOP)/frameworks/av/media/libstagefright/include             \
 	$(TOP)/frameworks/av/media/libstagefright/mpeg2ts             \
 	$(TOP)/frameworks/av/media/libstagefright/rtsp                \
-	$(TOP)/hardware/qcom/media-caf-new/mm-core/inc                \
-	$(TOP)/hardware/qcom/display-caf-new/libgralloc               \
-	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+	$(TOP)/hardware/qcom/media-caf-new/mm-core/inc
 
 ifeq ($(PLATFORM_SDK_VERSION), 18)
   LOCAL_CFLAGS += -DANDROID_JB_MR2
 endif
-
-LOCAL_CFLAGS += -DBFAMILY_TARGET
 
 LOCAL_MODULE:= libdashplayer
 
